5f37eb8237da60876c9104b434dc91b88ae5e8c3
Ashu <ashutosh.softronet@gmail.com>
2019-12-16 12:35:59 +0530

    Done changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'changeDetectionCore';
+  _time;
+  constructor(private zone: NgZone) {
+    this._time = Date.now();
+    zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this._time = Date.now();
+      }, 1);
+    });
+  }
+
+  public testFunction = () => {
+    console.log('Hey I am the testFunction');
+  }
+
+  get timeTemplateRef() {
+    // console.log('1');
+    return this._time;
+  }
+
 }
